d6f8a85e20dc91dec8a5e8172b6eb8f29f850df1
MeatTom <meattom@yandex.ru>
2023-03-11 21:17:12 +0300

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {useEffect} from "react";
+const telegram = window.Telegram.WebApp;
+function App() {
+
+    useEffect(() => {
+        telegram.ready()
+    },[])
+
+    const onClose = () => {
+        telegram.Close()
+    }
+
+  return (
+    <div className="App">
+        работаем
+        <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;

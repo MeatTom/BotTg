442e2d8a5ffac57a3e1c7484067f15ed9c9adb99
MeatTom <meattom@yandex.ru>
2023-03-13 22:09:17 +0300

    .

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -4,14 +4,11 @@ import {useTelegram} from "../../hooks/telegram";
 import HeaderStyle from './header.module.css'
 
 const Header = () => {
-    const {user, onClose} = useTelegram()
+    const {onClose} = useTelegram()
 
     return (
         <div className={HeaderStyle.header}>
             <Button onClick={onClose}>Закрыть</Button>
-            <p className={HeaderStyle.username}>
-                Пользователь: {user?.username}
-            </p>
         </div>
     );
 };

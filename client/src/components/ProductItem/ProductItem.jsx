a689724d8226282b1969422acb4ae26c36385fc5
MeatTom <meattom@yandex.ru>
2023-03-14 22:15:22 +0300

    update

--- client/src/components/ProductItem/ProductItem.jsx
+++ client/src/components/ProductItem/ProductItem.jsx
@@ -12,14 +12,15 @@ const ProductItem = ({product, onAdd, onCardClick}) => {
     }
 
     return (
-        <div className={ProductItemStyle.product} onClick={onCardClickHandler}>
+        <div className={ProductItemStyle.product}>
+            <div onClick={onCardClickHandler}>
             <div className={ProductItemStyle.category}>{product.category}</div>
             <div>
                 <img src = {product.img} alt={product.title}/>
             </div>
             <div className={ProductItemStyle.title}>{product.title}</div>
             <div className={ProductItemStyle.description}>{product.description}</div>
-
+            </div>
         <Button className={ProductItemStyle.add_button} onClick={onAddHandler}>
             Заказать
         </Button>

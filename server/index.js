b0a8f955cdc40f6279a40c3bf941119a9109aba3
MeatTom <meattom@yandex.ru>
2023-03-19 23:51:47 +0300

    update

--- server/index.js
+++ server/index.js
@@ -33,12 +33,14 @@ syncDatabase().then(() => {
     });
 });
 
-app.options('*', cors())
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-    credentials: true
-}));
+//app.options('*', cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 
 app.use('/admin', adminRouter);
